49155ae9b0eab1109608c534d62fc9b586fa02e6
Ivan Vilashek <ivan.vilashek@gmail.com>
2023-04-08 01:17:00 +0300

    Implemented features

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,9 +1,20 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Login, NotFound } from './pages';
-import App from './App';
+import { ROUTES } from './constants';
 
 const AppRouter = () => {
-  return <BrowserRouter></BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={ROUTES.home}
+          Component={() => <h1 style={{ textAlign: 'center' }}>Home Page</h1>}
+        />
+        <Route path={ROUTES.login} Component={() => <Login />} />
+        <Route path={ROUTES.notFound} Component={() => <NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default AppRouter;
