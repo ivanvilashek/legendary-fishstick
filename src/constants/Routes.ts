b14af19874f885da57bf8582b0783aec4cec6700
Ivan Vilashek <ivan.vilashek@gmail.com>
2023-04-07 14:39:45 +0300

    Edited Routes.ts file

--- src/constants/Routes.ts
+++ src/constants/Routes.ts
@@ -1,8 +1,10 @@
-export const Routes = {
-    return (
-        <Routes>
-        <Route path="/login" Component={() => <Login />} />
-        <Route path="*" Component={() => <NotFound />} />
-      </Routes>
-    )
-}
+// import { Routes, Route } from "react-router-dom"
+
+// export const Routes = () => {
+//     return (
+//         <Routes>
+//         <Route path="/login" Component={() => <Login />} />
+//         <Route path="*" Component={() => <NotFound />} />
+//       </Routes>
+//     )
+// }
