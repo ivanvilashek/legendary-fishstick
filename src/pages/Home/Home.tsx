dae29328b663f9c63967bc44d2850fd1e352b064
Ivan Vilashek <ivan.vilashek@gmail.com>
2023-04-10 12:16:47 +0000

    Updated Home.tsx and Login.tsx files

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,9 @@
+import {Typography, Space, Button} from 'antd';
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { ROUTES } from "../../constants";
+
+const {Title} = Typography;
 
 export const Home = () => {
   const navigate = useNavigate();
@@ -9,6 +12,16 @@ export const Home = () => {
       navigate(ROUTES.LOGIN);
     }
   });
+  
+  const logoutHandler = () => {
+    localStorage.setItem("token",false)
+    navigate(ROUTES.LOGIN)
+  }
 
-  return <h1 style={{ textAlign: "center" }}>Home page</h1>;
+  return (
+    <Space size="24" align="center" direction="vertical">
+      <Title level="1">Home page</Title>
+      <Button type="primary" onClick={logoutHandler}>Logout</Button>
+    </Space>
+  )
 };
