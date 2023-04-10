8a8d8c9ce1706e574dbdbc492210295a866598a0
ivanvilashek <120437487+ivanvilashek@users.noreply.github.com>
2023-04-10 11:29:59 +0300

    Update NotFound.tsx

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,3 +1,1 @@
-const NotFound = () => <h1 style={{ textAlign: 'center' }}>404 Not Found</h1>;
-
-export default NotFound;
+export const NotFound = () => <h1 style={{ textAlign: 'center' }}>404 Not Found</h1>;
